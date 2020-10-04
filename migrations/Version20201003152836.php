4b5f497eec235b26561fb194c12f223fb658310e
MarieBonifacio <mariebonifacio.pro@gmail.com>
2020-10-04 12:14:17 +0200

    fix: pretty

--- migrations/Version20201003152836.php
+++ migrations/Version20201003152836.php
@@ -12,12 +12,12 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20201003152836 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE language (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -29,7 +29,7 @@ final class Version20201003152836 extends AbstractMigration
         $this->addSql('ALTER TABLE snippet_tag ADD CONSTRAINT FK_485C6E1ABAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE snippet DROP FOREIGN KEY FK_961C8CD582F1BAF4');
